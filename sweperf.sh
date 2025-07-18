4a639cb0f45a9280428b2da61e5a77c8b57dcf61
Jeffrey Ma <jeffreyma@g.harvard.edu>
2025-07-18 07:37:43 +0000

    updated

--- sweperf.sh
+++ sweperf.sh
@@ -1,5 +1,4 @@
 sweagent run-batch \
     --config config/default_sweperf.yaml \
-    --agent.model.name gemini/gemini-2.5-flash \
     --agent.model.per_instance_cost_limit 2.00 \
     --num_workers 4 \
