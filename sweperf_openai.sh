f58f8b55b5dd38ac2c984ae4288c752c0c84645e
Jeffrey Ma <jeffreyma@g.harvard.edu>
2025-08-20 20:43:35 +0000

    fix configs

--- sweperf_openai.sh
+++ sweperf_openai.sh
@@ -1,4 +1,4 @@
 sweagent run-batch \
-    --config config/default_sweperf_claude.yaml \
+    --config config/default_sweperf_openai.yaml \
     --num_workers 4 \
     --instances.deployment.docker_args=--memory=16g
